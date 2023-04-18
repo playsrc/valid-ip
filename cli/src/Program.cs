8d0668b116e7faa13294ca5008b552797588252b
Mateus Abelli <mateusabelli@gmail.com>
2023-04-18 20:17:38 -0300

    Update the CLI

--- cli/src/Program.cs
+++ cli/src/Program.cs
@@ -1,66 +1,150 @@
 ﻿using System.CommandLine;
 
-namespace ValidIpCli;
+namespace ValidIp;
+
 class Program
 {
     static int Main(string[] args)
     {
         var isV4Option = new Option<string>(
-        name: "--v4",
-        description: "Verify if the argument is a valid IP v4 address"
+            name: "--v4",
+            description: "Test if <ip> is a valid IPv4"
         );
-
 
         var isV6Option = new Option<string>(
-        name: "--v6",
-        description: "Verify if the argument is a valid IP v6 address"
+            name: "--v6",
+            description: "Test if <ip> is a valid IPv6"
         );
 
-        isV4Option.ArgumentHelpName = "ip address";
-        isV6Option.ArgumentHelpName = "ip address";
+        isV4Option.ArgumentHelpName = "ip";
+        isV6Option.ArgumentHelpName = "ip";
 
-        // TODO
-        var rootCommand = new RootCommand("Program desc...");
+        var rootCommand = new RootCommand(
+            "Validate or generate random IP addresses.\n"
+            + "Example usage: valid-ip --v4 8.8.8.8"
+        );
 
         rootCommand.AddOption(isV4Option);
         rootCommand.AddOption(isV6Option);
 
         rootCommand.AddValidator(result =>
         {
-            if (result.FindResultFor(isV4Option) is not null &&
-                result.FindResultFor(isV6Option) is not null)
+            if (
+                result.FindResultFor(isV4Option) is not null
+                && result.FindResultFor(isV6Option) is not null
+            )
             {
                 result.ErrorMessage = "You cannot use options --v4 and --v6 together";
             }
         });
 
         rootCommand.SetHandler(
-        (ipV4, ipV6) =>
-        {
-            if (ipV4 is not null)
-                OnValidateIpV4(ipV4);
+            (ipV4, ipV6) =>
+            {
+                if (ipV4 is not null)
+                {
+                    Validate.IpV4(ipV4);
+                    return;
+                }
 
-            if (ipV6 is not null)
-                OnValidateIpV6(ipV6);
-        },
-        isV4Option, isV6Option
+                if (ipV6 is not null)
+                {
+                    Validate.IpV6(ipV6);
+                    return;
+                }
+                else
+                    rootCommand.Invoke("--help");
+            },
+            isV4Option,
+            isV6Option
         );
 
-        if (args.Length == 0)
-            return rootCommand.Invoke("--help");
+        var generateV4Option = new Option<int>(
+            name: "--v4",
+            description: "Generate <amount> of IPv4 addresses"
+        );
+
+        var generateV6Option = new Option<int>(
+            name: "--v6",
+            description: "Generate <amount> of IPv6 addresses"
+        );
+
+        generateV4Option.ArgumentHelpName = "amount";
+        generateV6Option.ArgumentHelpName = "amount";
+
+        var generateCommand = new Command(
+            name: "generate",
+            description: "Generate random IP addresses.\n"
+                + "Example usage: valid-ip generate --v4 8"
+        );
+
+        generateCommand.AddOption(generateV4Option);
+        generateCommand.AddOption(generateV6Option);
+
+        generateCommand.AddAlias("gen");
+
+        rootCommand.AddCommand(generateCommand);
+
+        generateCommand.AddValidator(result =>
+        {
+            if (result.FindResultFor(generateV4Option) is not null
+                && result.FindResultFor(generateV6Option) is not null)
+            {
+                result.ErrorMessage = "You cannot use options --v4 and --v6 together";
+            }
+        });
+
+        generateCommand.SetHandler(
+            (ipV4Amount, ipV6Amount) =>
+            {
+                if (ipV4Amount is not 0)
+                {
+                    Generate.IpV4(ipV4Amount);
+                    return;
+                }
+
+                if (ipV6Amount is not 0)
+                {
+                    Generate.IpV6(ipV6Amount);
+                    return;
+                }
+                else
+                    generateCommand.Invoke("--help");
+            },
+            generateV4Option,
+            generateV6Option
+        );
 
         return rootCommand.Invoke(args);
     }
+}
 
-    static void OnValidateIpV4(string ipV4)
+class Validate
+{
+    public static void IpV4(string ipV4)
     {
         // TODO
-        Console.WriteLine("THE IP V4 ENTERED: " + ipV4);
+        Console.WriteLine($"Validating... {ipV4} IPv4");
     }
 
-    static void OnValidateIpV6(string ipV6)
+    public static void IpV6(string ipV6)
     {
         // TODO
-        Console.WriteLine("THE IP V6 ENTERED: " + ipV6);
+        Console.WriteLine($"Validating... {ipV6} IPv6");
+    }
+}
+
+class Generate
+{
+    public static void IpV4(int generateAmount)
+    {
+        // TODO
+        Console.WriteLine($"Generating... {generateAmount} x ipV4");
+    }
+
+    public static void IpV6(int generateAmount)
+    {
+        // TODO
+        Console.WriteLine($"Generating... {generateAmount} x ipV6");
     }
 }
