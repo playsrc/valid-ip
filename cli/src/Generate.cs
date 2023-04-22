f2dace460297466c9ae99a4a26e27c81058ebe27
Mateus Abelli <mateusabelli@gmail.com>
2023-04-22 09:58:45 -0300

    Extract CLI methods

---
+++ cli/src/Generate.cs
@@ -0,0 +1,16 @@
+namespace ValidIp;
+
+class Generate
+{
+    public static void IpV4(int generateAmount)
+    {
+        // TODO
+        Console.WriteLine($"Generating... {generateAmount} x ipV4");
+    }
+
+    public static void IpV6(int generateAmount)
+    {
+        // TODO
+        Console.WriteLine($"Generating... {generateAmount} x ipV6");
+    }
+}
---
+++ cli/src/Validate.cs
@@ -0,0 +1,16 @@
+namespace ValidIp;
+
+class Validate
+{
+    public static void IpV4(string ipV4)
+    {
+        // TODO
+        Console.WriteLine($"Validating... {ipV4} IPv4");
+    }
+
+    public static void IpV6(string ipV6)
+    {
+        // TODO
+        Console.WriteLine($"Validating... {ipV6} IPv6");
+    }
+}
