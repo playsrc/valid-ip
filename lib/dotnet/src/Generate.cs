a95793cffcd5d5d60c650469176f81e0e7a66a07
Mateus Abelli <mateusabelli@gmail.com>
2023-04-22 18:02:06 -0300

    Implement library structure

---
+++ lib/dotnet/src/Generate.cs
@@ -0,0 +1,16 @@
+namespace ValidIp;
+
+public class Generate
+{
+    public static string IpV4(int generateAmount)
+    {
+        // TODO
+        return $"Generating... {generateAmount} x ipV4";
+    }
+
+    public static string IpV6(int generateAmount)
+    {
+        // TODO
+        return $"Generating... {generateAmount} x ipV6";
+    }
+}
---
+++ lib/dotnet/src/Validate.cs
@@ -0,0 +1,16 @@
+namespace ValidIp;
+
+public class Validate
+{
+    public static string IpV4(string ipV4)
+    {
+        // TODO
+        return $"Validating... {ipV4} IPv4";
+    }
+
+    public static string IpV6(string ipV6)
+    {
+        // TODO
+        return $"Validating... {ipV6} IPv6";
+    }
+}
